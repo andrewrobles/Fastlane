e6996975ae920106695ddee988fa80c7c523a5c5
Andrew Robles <andrewrobles@verizon.net>
2023-11-17 19:00:00 -0800

    Align links horizontally

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-	<router-link to="/1">Psalm 1</router-link><br>
-	<router-link to="/2">Psalm 2</router-link><br>
-	<router-link to="/3">Psalm 3</router-link><br>
+	<div>
+		<router-link to="/1" style="margin-right:10px;">Psalm 1</router-link>
+		<router-link to="/2" style="margin-right:10px;">Psalm 2</router-link>
+		<router-link to="/3" style="margin-right:10px;">Psalm 3</router-link>
+	</div>
 	<router-view></router-view>
 </template>
