35d4d771af99d76181e3b7ab7a11e6c051c94f72
Andrew Robles <andrewrobles@verizon.net>
2023-11-20 11:43:22 -0800

    Capitalize Lord

--- web/src/PsalmOne.vue
+++ web/src/PsalmOne.vue
@@ -2,7 +2,7 @@
 Blessed is the man that walketh not in the counsel of the ungodly,<br>
 Nor standeth in the way of sinners,<br>
 Nor sitteth in the seat of the scornful<br>
-But his delight is in the law of the Lord;<br>
+But his delight is in the law of the LORD;<br>
 And in his law doth he meditate day and night.<br>
 And he shall be like a tree planted by the rivers of water,<br>
 That bringeth forth his fruit in his season;<br>
@@ -12,6 +12,6 @@ The ungodly are not so:<br>
 But are like the chaff which the wind driveth away.<br>
 Therefore the ungodly shall not stand in the judgement,<br>
 Nor sinners in the congregation of the righteous<br>
-For the Lord knoweth the way of the righteous:<br>
+For the LORD knoweth the way of the righteous:<br>
 But the way of the ungodly shall perish.<br>
 </template>
