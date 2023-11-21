a727e5e7c71f3d970b38866de57e8d0933338906
Andrew Robles <andrewrobles@verizon.net>
2023-11-20 19:36:52 -0800

    Add Psalm 1 to API

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-	return "<p>Hello, World!</p>"
+@app.route("/1")
+def psalm_1():
+	return render_template('1.html')
