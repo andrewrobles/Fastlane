7e45b1128c2195f180276e7f500c9bc5a64033bd
Andrew Robles <andrewrobles@verizon.net>
2023-11-21 06:56:08 -0800

    Add Psalms 2-6 to API

--- api/index.py
+++ api/index.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route("/1")
 def psalm_1():
 	return render_template('1.html')
+
+@app.route("/2")
+def psalm_2():
+	return render_template('2.html')
+
+@app.route("/3")
+def psalm_3():
+	return render_template('3.html')
+
+@app.route("/4")
+def psalm_4():
+	return render_template('4.html')
+
+@app.route("/5")
+def psalm_5():
+	return render_template('5.html')
+
+@app.route("/6")
+def psalm_6():
+	return render_template('6.html')
