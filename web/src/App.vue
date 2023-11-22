01f45533810ae45cea4b80b0e662ad8004ed946d
Andrew Robles <andrewrobles@verizon.net>
2023-11-22 06:55:16 -0800

    Align links in rows of 5

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,7 +3,7 @@
 <router-link to="/2" class="link">Psalm 2</router-link>
 <router-link to="/3" class="link">Psalm 3</router-link>
 <router-link to="/4" class="link">Psalm 4</router-link>
-<router-link to="/5" class="link">Psalm 5</router-link>
+<router-link to="/5" class="link">Psalm 5</router-link><br>
 <router-link to="/6" class="link">Psalm 6</router-link>
 <router-link to="/7" class="link">Psalm 7</router-link>
 <br>
